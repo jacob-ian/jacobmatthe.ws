d7473692df427cbe756e6395c1576bbe0f12b642
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-06-23 14:20:21 +1000

    Add db functions for getting drafts

--- backend/src/db/posts.rs
+++ backend/src/db/posts.rs
@@ -18,13 +18,15 @@ pub struct Post {
     published_at: Option<DateTime<Utc>>,
 }
 
+/// Gets all posts (drafts and published)
 pub async fn get_posts(pool: &PgPool) -> Result<Vec<Post>, errors::Error> {
     sqlx::query_as!(
         Post,
         "
             SELECT id,author_id,stub,title,description,content,created_at,updated_at,published_at 
             FROM \"post\"
-            WHERE deleted_at IS NULL;
+            WHERE deleted_at IS NULL
+            ORDER BY created_at DESC;
         ",
     )
     .fetch_all(pool)
@@ -32,6 +34,45 @@ pub async fn get_posts(pool: &PgPool) -> Result<Vec<Post>, errors::Error> {
     .map_err(|err| errors::Error::from_sqlx(err, "Post"))
 }
 
+/// Gets all published posts
+pub async fn get_published_posts(pool: &PgPool) -> Result<Vec<Post>, errors::Error> {
+    sqlx::query_as!(
+        Post,
+        "
+            SELECT id,author_id,stub,title,description,content,created_at,updated_at,published_at 
+            FROM \"post\"
+            WHERE
+                deleted_at IS NULL
+                AND published_at IS NOT NULL
+            ORDER BY
+                published_at DESC;
+        ",
+    )
+    .fetch_all(pool)
+    .await
+    .map_err(|err| errors::Error::from_sqlx(err, "Post"))
+}
+
+/// Gets all drafts
+pub async fn get_drafts(pool: &PgPool) -> Result<Vec<Post>, errors::Error> {
+    sqlx::query_as!(
+        Post,
+        "
+            SELECT id,author_id,stub,title,description,content,created_at,updated_at,published_at 
+            FROM \"post\"
+            WHERE
+                deleted_at IS NULL
+                AND published_at IS NULL
+            ORDER BY
+                created_at DESC;
+        ",
+    )
+    .fetch_all(pool)
+    .await
+    .map_err(|err| errors::Error::from_sqlx(err, "Post"))
+}
+
+/// Gets a published post by stub
 pub async fn get_post_by_stub(pool: &PgPool, stub: String) -> Result<Post, errors::Error> {
     sqlx::query_as!(
         Post,
@@ -40,9 +81,28 @@ pub async fn get_post_by_stub(pool: &PgPool, stub: String) -> Result<Post, error
             FROM \"post\"
             WHERE
                 deleted_at IS NULL
+                AND published_at IS NOT NULL
                 AND stub = $1;
         ",
         stub
+    )
+    .fetch_one(pool)
+    .await
+    .map_err(|err| errors::Error::from_sqlx(err, "Post"))
+}
+
+/// Gets a post by ID
+pub async fn get_post_by_id(pool: &PgPool, id: uuid::Uuid) -> Result<Post, errors::Error> {
+    sqlx::query_as!(
+        Post,
+        "
+            SELECT id,author_id,stub,title,description,content,created_at,updated_at,published_at 
+            FROM \"post\"
+            WHERE
+                deleted_at IS NULL
+                AND id = $1;
+        ",
+        id
     )
     .fetch_one(pool)
     .await
