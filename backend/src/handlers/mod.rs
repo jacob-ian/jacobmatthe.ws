3de8153cfef243ce1599a57c065eddac2998265d
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-06-28 20:31:45 +1000

    Make handler modules private

--- backend/src/handlers/mod.rs
+++ backend/src/handlers/mod.rs
@@ -3,11 +3,11 @@ use actix_web::{
     HttpResponse, Responder,
 };
 
-pub mod auth;
-pub mod drafts;
-pub mod posts;
-pub mod uploads;
-pub mod users;
+mod auth;
+mod drafts;
+mod posts;
+mod uploads;
+mod users;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().body("Hello world")
