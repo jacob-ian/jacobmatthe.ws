dc389015c1a48a1bb8ea845df58d27796cc680b7
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-07-15 18:40:54 +1000

    Add rust website

---
+++ website/src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{http::header::ContentType, web, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    return HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body("<html><body><h1>You made it!</h1></body></html>");
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let server = HttpServer::new(move || App::new().route("/", web::get().to(index)))
+        .bind("0.0.0.0:4000")?
+        .run();
+
+    println!("\n-------\n\nSuccessfully started jacobmatthe.ws!\n");
+    println!("Listening on {}:{}\n\n-------", "0.0.0.0", 4000);
+
+    return server.await;
+}
