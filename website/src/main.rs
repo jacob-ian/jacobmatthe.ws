33485a242361e82e2286ce7446e10ef438697168
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-07-15 19:06:10 +1000

    Try embed images from uploads

--- website/src/main.rs
+++ website/src/main.rs
@@ -1,5 +1,11 @@
 use actix_web::{http::header::ContentType, web, App, HttpResponse, HttpServer, Responder};
 
+async fn page() -> impl Responder {
+    return HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body("<html><body><h1>Page</h1><img src=\"/uploads/test.jpg\"/></body></html>");
+}
+
 async fn index() -> impl Responder {
     return HttpResponse::Ok()
         .content_type(ContentType::html())
@@ -8,9 +14,13 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let server = HttpServer::new(move || App::new().route("/", web::get().to(index)))
-        .bind("0.0.0.0:4000")?
-        .run();
+    let server = HttpServer::new(move || {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/page", web::get().to(page))
+    })
+    .bind("0.0.0.0:4000")?
+    .run();
 
     println!("\n-------\n\nSuccessfully started jacobmatthe.ws!\n");
     println!("Listening on {}:{}\n\n-------", "0.0.0.0", 4000);
