7fece101be54e8c18b5dbd39c216fa49a234e42b
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-07-17 16:51:33 +1000

    Add config to startup

--- website/src/main.rs
+++ website/src/main.rs
@@ -1,14 +1,23 @@
+use std::process;
+
 use actix_web::{App, HttpServer};
-use website::pages;
+use website::{config::Config, pages};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let config = match Config::from_toml() {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("Could not read config: {}", e);
+            process::exit(1)
+        }
+    };
     let server = HttpServer::new(move || App::new().configure(pages::config))
-        .bind("0.0.0.0:4000")?
+        .bind((config.host.clone(), config.port))?
         .run();
 
     println!("\n-------\n\nSuccessfully started jacobmatthe.ws!\n");
-    println!("Listening on {}:{}\n\n-------", "0.0.0.0", 4000);
+    println!("Listening on {}:{}\n\n-------", config.host, config.port);
 
     return server.await;
 }
