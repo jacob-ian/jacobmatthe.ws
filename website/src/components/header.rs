4270ccacb4e93d5dbed741d23f9c475c27036dd2
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-07-19 20:51:14 +1000

    Proper centering for navbar

--- website/src/components/header.rs
+++ website/src/components/header.rs
@@ -4,15 +4,15 @@ pub fn new() -> String {
     <header class="flex flex-row justify-center sticky top-0 bg-zinc-900 text-sky-400 pt-10">
         <div class="flex-1 border-b border-sky-600 relative max-w-5xl">
             <div class="w-full flex flex-row items-center max-w-5xl px-5 absolute -bottom-3">
-                <a href="/" alt="Home" class="text-md bg-zinc-900 px-1">jacobmatthe.ws</a>
-                <div class="flex-1"></div>
-                <nav class="flex flex-row justify-start items-center gap-5">
+                <div class="flex-1 flex flex-row justify-start">
+                    <a href="/" alt="Home" class="text-md bg-zinc-900 px-1">jacobmatthe.ws</a>
+                </div>
+                <nav class="flex-1 flex flex-row justify-center items-center gap-5">
                     {home}
                     {blog}
                     {about}
                 </nav>
-                <div class="flex-1"></div>
-                <div class="flex flex-row justify-end">
+                <div class="flex-1 flex flex-row justify-end">
                     <button class="bg-zinc-900 px-1" alt="Search">Search</button>
                 </div>
             </div>
