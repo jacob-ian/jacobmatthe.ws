6252c5b05b20d0fcde671062473d591be70ca22f
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-07-19 20:57:04 +1000

    Add blog page

---
+++ website/src/pages/blog.rs
@@ -0,0 +1,16 @@
+use actix_web::Responder;
+
+use crate::{components::article, html::HtmlResponse};
+
+pub async fn blog() -> impl Responder {
+    return HtmlResponse::builder()
+        .title("Blog | jacobmatthe.ws")
+        .body(article::new(
+            r#"
+        <h1>Blog</h1>
+        <p>Posts go here</p>
+    "#
+            .to_string(),
+        ))
+        .build();
+}
--- website/src/pages/mod.rs
+++ website/src/pages/mod.rs
@@ -1,10 +1,12 @@
 use actix_web::web::{self, ServiceConfig};
 
 mod about;
+mod blog;
 mod home;
 
 /// Configure the webpage routes
 pub fn config(cfg: &mut ServiceConfig) {
+    cfg.route("/blog", web::get().to(blog::blog));
     cfg.route("/about", web::get().to(about::about));
     cfg.route("/", web::get().to(home::home));
 }
