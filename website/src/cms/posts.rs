8404d58575711e0277d63526a50f193aee024d8a
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-07-25 17:09:42 +1000

    Finish post type, localise get_post_by_stub call

--- website/src/cms/posts.rs
+++ website/src/cms/posts.rs
@@ -1,21 +1,16 @@
+use chrono::{DateTime, Utc};
 use serde::Deserialize;
-
-use crate::errors::Error;
-
-use super::Client;
 
 #[allow(dead_code)]
 #[derive(Deserialize, Debug)]
 pub struct Post {
     pub id: String,
+    pub author_id: String,
     pub stub: String,
     pub title: String,
-}
-
-pub async fn get_post_by_stub(client: &Client, stub: &str) -> Result<Post, Error> {
-    return client
-        .get()
-        .path(format!("posts/{}", stub))
-        .json::<Post>()
-        .await;
+    pub description: String,
+    pub content: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    pub published_at: Option<DateTime<Utc>>,
 }
--- website/src/pages/post.rs
+++ website/src/pages/post.rs
@@ -1,17 +1,25 @@
 use actix_web::web;
 
 use crate::{
-    cms::{self, Client},
+    cms::{posts::Post, Client},
     components::article,
     errors::Error,
     html::HtmlResponse,
 };
 
+pub async fn get_post_by_stub(client: &Client, stub: &str) -> Result<Post, Error> {
+    return client
+        .get()
+        .path(format!("posts/{}", stub))
+        .json::<Post>()
+        .await;
+}
+
 pub async fn post(
     client: web::Data<Client>,
     stub: web::Path<String>,
 ) -> Result<HtmlResponse, Error> {
-    let post = cms::posts::get_post_by_stub(&client, &stub).await?;
+    let post = get_post_by_stub(&client, &stub).await?;
 
     return Ok(HtmlResponse::builder()
         .title(format!("{} | Jacob Matthews", post.title))
