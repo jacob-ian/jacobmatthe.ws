85ebf26e0fa411831b4b2f48119ea632c8783559
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-07-26 21:02:52 +1000

    Add posts links

--- website/src/pages/blog.rs
+++ website/src/pages/blog.rs
@@ -1,20 +1,45 @@
-use actix_web::Responder;
+use actix_web::web;
 
-use crate::{components::article, html::HtmlResponse};
+use crate::{
+    cms::{posts::Post, Client},
+    components::title,
+    errors::Error,
+    html::HtmlResponse,
+};
 
-pub async fn blog() -> impl Responder {
-    return HtmlResponse::builder()
+async fn get_posts(client: &Client) -> Result<Vec<Post>, Error> {
+    return client
+        .get()
+        .path(String::from("posts"))
+        .json::<Vec<Post>>()
+        .await;
+}
+
+pub async fn blog(client: web::Data<Client>) -> Result<HtmlResponse, Error> {
+    let posts = get_posts(&client)
+        .await?
+        .into_iter()
+        .map(|p| {
+            format!(
+                r#"<a href="/{stub}">{name}</a>"#,
+                stub = p.stub,
+                name = p.title
+            )
+        })
+        .collect::<Vec<String>>()
+        .join("\n")
+        .to_string();
+
+    return Ok(HtmlResponse::builder()
         .title(String::from("Blog | Jacob Matthews"))
-        .body(
-            article::builder()
-                .title(String::from("Blog"))
-                .content(
-                    r#"
-<p>Posts go here</p>
-                    "#
-                    .to_string(),
-                )
-                .render(),
-        )
-        .build();
+        .body(format!(
+            r#"
+            {title}
+            <h2 class="my-5 pb-2 border-b border-sky-600">Latest</h2>
+            {posts}
+            "#,
+            title = title::render("Blog"),
+            posts = posts
+        ))
+        .build());
 }
