95464d8cdc2445f2aa514691f0e27baeb285ab8f
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-08-04 11:33:34 +1000

    Add html lang

--- website/src/components/page.rs
+++ website/src/components/page.rs
@@ -7,7 +7,7 @@ pub fn from_response(res: &HtmlResponse) -> String {
     return format!(
         r#"
         <!DOCTYPE html>
-        <html>
+        <html lang="en">
             <head>
                 <meta charset="utf-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
