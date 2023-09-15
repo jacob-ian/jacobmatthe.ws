d46911508308a817aee2b673b71175ee3c0ca625
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-09-15 17:20:42 +1000

    Refactor all posts component

--- website/src/components/all_posts.rs
+++ website/src/components/all_posts.rs
@@ -9,7 +9,42 @@ use crate::{
 
 pub async fn render(client: &Client) -> Result<String, Error> {
     let posts = get_all_posts(client).await?;
-    let list = group_by_year_month(posts)
+    let grouped = group_by_year_month(posts);
+    let list = render_collapsable_list(grouped);
+
+    return Ok(format!(
+        r#"
+        <h2 class="my-5 text-lg">All Posts:</h2>
+        {list}
+        "#,
+        list = list
+    ));
+}
+
+// TODO: This will become really inefficient - request less fields
+async fn get_all_posts(client: &Client) -> Result<Vec<Post>, Error> {
+    return client
+        .get()
+        .path(String::from("posts"))
+        .json::<Vec<Post>>()
+        .await;
+}
+
+fn group_by_year_month(posts: Vec<Post>) -> HashMap<i32, HashMap<String, Vec<Post>>> {
+    let mut map: HashMap<i32, HashMap<String, Vec<Post>>> = HashMap::new();
+    for post in posts {
+        let year = post.published_at.year();
+        let month = post.published_at.format("%B").to_string();
+
+        let year_group = map.entry(year).or_insert(HashMap::new());
+        let month_group = year_group.entry(month).or_insert(Vec::new());
+        month_group.push(post);
+    }
+    return map;
+}
+
+fn render_collapsable_list(posts: HashMap<i32, HashMap<String, Vec<Post>>>) -> String {
+    return posts
         .into_iter()
         .map(|(y, m)| {
             return format!(
@@ -56,34 +91,4 @@ pub async fn render(client: &Client) -> Result<String, Error> {
         })
         .collect::<Vec<String>>()
         .join("\n");
-
-    return Ok(format!(
-        r#"
-        <h2 class="my-5 text-lg">All Posts:</h2>
-        {list}
-        "#,
-        list = list
-    ));
-}
-
-// TODO: This will become really inefficient - request less fields
-async fn get_all_posts(client: &Client) -> Result<Vec<Post>, Error> {
-    return client
-        .get()
-        .path(String::from("posts"))
-        .json::<Vec<Post>>()
-        .await;
-}
-
-fn group_by_year_month(posts: Vec<Post>) -> HashMap<i32, HashMap<String, Vec<Post>>> {
-    let mut map: HashMap<i32, HashMap<String, Vec<Post>>> = HashMap::new();
-    for post in posts {
-        let year = post.published_at.year();
-        let month = post.published_at.format("%B").to_string();
-
-        let year_group = map.entry(year).or_insert(HashMap::new());
-        let month_group = year_group.entry(month).or_insert(Vec::new());
-        month_group.push(post);
-    }
-    return map;
 }
