c7e8b143a393f75a74d62b58a1110be7785d2e77
Jacob Ian Matthews <jacob@jacobian.com.au>
2023-09-24 20:06:23 +1000

    Add favicon

--- website/src/components/page.rs
+++ website/src/components/page.rs
@@ -13,6 +13,7 @@ pub fn from_response(res: &HtmlResponse) -> String {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <title>{title}</title>
                 <meta name="description" content="{description}" />
+                <link rel="icon" type"image/x-icon" href="/static/favicon.ico">
 
                 <!-- Fonts -->
                 <link rel="preconnect" href="https://fonts.googleapis.com">
